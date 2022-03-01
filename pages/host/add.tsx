5cb4a42734c4f8e3ecb4a5bf46605aa5638663e6
Jakub Kłobus <jakub.klobus@outlook.com>
2022-03-01 23:02:38 +0100

    feat: second section and i18 usage

--- pages/host/add.tsx
+++ pages/host/add.tsx
@@ -1,5 +1,12 @@
 import { CompositionAppBody } from "../../src/components/Compositions";
 import AddAccommodationAdvancedForm from "../../src/components/AddAccommodationAdvancedForm";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export const getServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale)),
+  },
+});
 
 export default function Account() {
   return (
