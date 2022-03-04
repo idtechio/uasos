df29862dd415d5c60921ef2b00b1ce9950915c86
malgorzatamaz <m.mazurek@selleo.com>
2022-03-04 12:44:55 +0100

    feedback

--- src/components/Forms/Autocomplete/Item/index.tsx
+++ src/components/Forms/Autocomplete/Item/index.tsx
@@ -4,9 +4,11 @@ import { Touchable, View, Text } from "./style";
 import { ItemProps } from "./types";
 
 const Autocomplete = ({ disabled, value, label, onPress }: ItemProps) => {
+  const onItemPress = () => onPress && onPress(value);
+
   return (
     <View>
-      <Touchable disabled={disabled} onPress={() => onPress && onPress(value)}>
+      <Touchable disabled={disabled} onPress={onItemPress}>
         <Text>{label}</Text>
       </Touchable>
     </View>
