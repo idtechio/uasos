3ffc19b08c0a9f3471b3777fc99e3048580dc8c6
Marcin Grzywaczewski <killavus@gmail.com>
2022-03-07 20:44:16 +0100

    fix translation issues (small wording issues); move ukrainian translation to a proper code (UA vs UK)

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     // https://www.localeplanet.com/icu/iso639.html
     // TODO: uncomment when translation will be done
-    locales: ["uk", "pl", "ru"], // "en", "de", "fr", "ro", "hu", "es", "cs", "sk", "it"],
+    locales: ["ua", "pl", "ru"], // "en", "de", "fr", "ro", "hu", "es", "cs", "sk", "it"],
     defaultLocale: "pl",
   },
 };
