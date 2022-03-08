b9a1235271e21fd7aeeafbc90994f25ab8e41b7c
Denis Slavik <denis.slavik@gmail.com>
2022-03-08 11:47:58 +0100

    Revert unnecessary changes after rebase II.

--- pages/api/hosts/add.ts
+++ pages/api/hosts/add.ts
@@ -2,7 +2,7 @@
 import { getSession } from "next-auth/react";
 import { publishMessage } from "../../../src/helpers/PubSub";
 
-export enum Boolean {
+enum Boolean {
   FALSE = "FALSE",
   TRUE = "TRUE",
 }
@@ -16,11 +16,11 @@ export interface HostProps {
   shelter_type: Array<string>;
   beds: number;
   acceptable_group_relations: Array<string>;
-  is_pregnant: Boolean;
-  is_with_disability: Boolean;
-  is_with_animal: Boolean;
-  is_with_elderly: Boolean;
-  is_ukrainian_nationality: Boolean;
+  ok_for_pregnant: Boolean;
+  ok_for_disabilities: Boolean;
+  ok_for_animals: Boolean;
+  ok_for_elderly: Boolean;
+  ok_for_any_nationality: Boolean;
   duration_category: Array<string>;
 }
 
