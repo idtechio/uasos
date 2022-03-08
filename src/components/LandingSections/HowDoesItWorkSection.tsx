7d38a4c4c3220eeaa863f08e1df54c24f56e20f1
Mateusz Wit <maateew@gmail.com>
2022-03-08 22:24:21 +0100

    feat: add favicon (#116)

--- src/components/LandingSections/HowDoesItWorkSection.tsx
+++ src/components/LandingSections/HowDoesItWorkSection.tsx
@@ -3,7 +3,7 @@ import { Text } from "react-native";
 import styled, { css, useTheme } from "styled-components/native";
 import SectionTitle from "../SectionTitle";
 import pathSVG from "../../../public/how-does-it-work-path.png";
-import { useMemo } from "react";
+import React, { useMemo } from "react";
 import Section from "../Section";
 
 const Image = styled.Image`
@@ -156,15 +156,15 @@ export function HowDoesItWorkSection() {
     return (
       <>
         {Array.from({ length: 5 }).map((_, index, arr) => (
-          <>
-            <PathItemMobileContainer key={index}>
+          <React.Fragment key={index}>
+            <PathItemMobileContainer>
               <PathItemDot>
                 <Text>{index + 1}</Text>
               </PathItemDot>
               <PathItemText>{t(`howDoesItWork.points.${index}`)}</PathItemText>
             </PathItemMobileContainer>
             {index !== arr.length - 1 && <PathDotConnector />}
-          </>
+          </React.Fragment>
         ))}
       </>
     );
