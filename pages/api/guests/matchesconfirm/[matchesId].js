ee85d2104ff14933f4299aed53130c775729a831
Łukasz Bieniek <bieniek.luk@gmail.com>
2022-03-09 21:22:52 +0100

    fix maches

--- pages/api/guests/matchesconfirm/[matchesId].js
+++ pages/api/guests/matchesconfirm/[matchesId].js
@@ -1,25 +1,20 @@
 import { getSession } from "next-auth/react";
 import { publishMessage } from "../../../../src/helpers/PubSub";
 
-//TODO: DRY pages/api/hosts/matchesconfirm/{matchesId}.js
+//TODO: DRY pages/api/guests/matchesconfirm/{matchesId}.js
 export default async function sendMatchesDecision(req, res) {
-  const session = await getSession({ req });
-  const { matchesId } = req.query;
-
   const {
-    query: { accepted },
+    query: { accepted, matchesId },
   } = req;
 
-  if (session) {
-    const topicNameOrId = process.env.TOPIC_MATCH_HOST;
-    const matches = {
-      matches_id: matchesId,
-      is_host: true,
-      accepted: accepted,
-    };
-    res.status(200).json(await publishMessage(topicNameOrId, matches));
-  } else {
-    res.status(401);
-  }
+  const topicNameOrId = process.env.TOPIC_MATCH_HOST;
+  const matches = {
+    matches_id: matchesId,
+    is_host: false,
+    accepted: accepted,
+  };
+  console.log(topicNameOrId, matches);
+  publishMessage(topicNameOrId, JSON.stringify(matches));
+  res.status(200).json({ ok: "ok" });
   res.end();
 }
