639b1576921ef332e6cf6ed09fb0cfe2091025f5
Artem Zhabinsky <artem.zhabinsky@start-up.house>
2022-03-18 11:31:34 +0100

    SOSUA-84 Add thumbnail

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import Head from "next/head";
-import { appWithTranslation } from "next-i18next";
+import { appWithTranslation, useTranslation } from "next-i18next";
 import { ThemeProvider as ThemeProviderWeb } from "styled-components";
 import { ThemeProvider as ThemeProviderNative } from "styled-components/native";
 import { primary } from "../src/style/theme.config";
@@ -14,6 +14,7 @@ import { AppProps } from "next/app";
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const getBreakPoint = useBreakPointGetter();
   const theme = useMemo(() => ({ ...primary, getBreakPoint }), [getBreakPoint]);
+  const { t } = useTranslation();
 
   useEffect(() => {
     init(process.env.NEXT_PUBLIC_G);
@@ -25,6 +26,13 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
       <SessionProvider session={session}>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta property="og:title" content={t("ogMeta.title")} />
+          <meta property="og:description" content={t("ogMeta.description")} />
+          <meta
+            property="og:image"
+            content="https://uasos.org/assets/fb_banner.png"
+          />
+          <meta property="og:image:type" content="image/png" />
         </Head>
         <ThemeProviderWeb theme={theme}>
           <ThemeProviderNative theme={theme}>
