edf11b53de18a8bb9788f913bcec8d16d9339b70
Aleksandra <aleksandra.karp1@gmail.com>
2022-03-18 22:00:51 +0100

    hungarian translation added

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   i18n: {
     // https://www.localeplanet.com/icu/iso639.html
     // TODO: uncomment when translation will be done
-    locales: ["ua", "pl", "ru", "en"], // "en", "de", "fr", "ro", "hu", "es", "cs", "sk", "it"],
+    locales: ["ua", "pl", "ru", "en", "hu"], // "en", "de", "fr", "ro", "hu", "es", "cs", "sk", "it"],
     defaultLocale: "pl",
     fallbackLng: ["en", "pl"], // will fallback to english and then to polish if there's no english translation
     returnEmptyString: false, // allow an empty value to count as invalid (by default is true)
