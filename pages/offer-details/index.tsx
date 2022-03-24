968ae4bbe1f1a9bb31bbef50fd01735d5cf2987c
Aleksandra <aleksandra.karp1@gmail.com>
2022-03-24 20:25:28 +0100

    back button in offer details route changed from /desktop to /dashboard

--- pages/offer-details/index.tsx
+++ pages/offer-details/index.tsx
@@ -56,7 +56,7 @@ export default function OfferDetails() {
         <InnerWrapper>
           <BackWrapper
             onPress={() => {
-              router.push("/desktop");
+              router.push("/dashboard");
             }}
           >
             <ArrowLeftIcon />
