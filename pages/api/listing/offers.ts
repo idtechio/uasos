51618e7bb4526d4586dc531adf579231cb349d84
Michal Szkodzinski <mszkodzinski@users.noreply.github.com>
2022-03-24 16:21:57 +0100

    SOSUA-142 code fix

--- pages/api/listing/offers.ts
+++ pages/api/listing/offers.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import withApiAuth, {
-  ApiAuthTokenDetails,
+import {
+  /* withApiAuth, */ ApiAuthTokenDetails,
 } from "../../../src/helpers/withAPIAuth";
 
 enum Boolean {
