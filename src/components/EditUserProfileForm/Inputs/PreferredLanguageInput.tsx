28b8b1f0a1558260025fe7b9f970c2478687a200
Łukasz <lukaszradecki404@gmail.com>
2022-03-25 13:27:08 +0100

    Controlled preferred language - edit user form

--- src/components/EditUserProfileForm/Inputs/PreferredLanguageInput.tsx
+++ src/components/EditUserProfileForm/Inputs/PreferredLanguageInput.tsx
@@ -1,20 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
+import { Controller, useFormContext } from "react-hook-form";
 import CountrySelect from "../../Forms/CountrySelect";
+import PhoneNumberPrefixSelect from "../../Forms/PhoneNumberPrefixSelect";
+import { EditProfileForm } from "../types";
 import { InputWrapper } from "./style";
 
 export default function PreferredLanguageInput() {
-  const [state, setState] = useState<string | string[]>("");
+  const { control } = useFormContext<EditProfileForm>();
+
   return (
     <InputWrapper
       label="Preferred language of communication"
       styles={{ container: { zIndex: 9999 } }}
     >
-      <CountrySelect
-        value={state}
-        onChange={setState}
-        placeholder="Preferred language of communication"
-        errorMsg="Error"
-        error={{} as any}
+      <Controller
+        control={control}
+        name="preferredLanguage"
+        render={({ field: { value, onChange }, fieldState: { error } }) => (
+          <CountrySelect
+            value={value || ""}
+            onChange={onChange}
+            placeholder="Preferred language of communication"
+            error={error}
+            errorMsg={error?.message}
+          />
+        )}
+      />
+
+      <Controller
+        control={control}
+        name="phonePrefix"
+        render={({ field: { value, onChange }, fieldState: { error } }) => (
+          <PhoneNumberPrefixSelect
+            value={value || ""}
+            onChange={onChange}
+            error={error}
+            errorMsg={error?.message}
+            placeholder="Prefix"
+          />
+        )}
       />
     </InputWrapper>
   );
