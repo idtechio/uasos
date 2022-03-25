38ce9024ad98d6e24e05ec3b59f3d63f9066db1e
Michal Szkodzinski <mszkodzinski@users.noreply.github.com>
2022-03-25 12:39:28 +0100

    SOSUA-140 param parse fix

--- pages/api/account/update.ts
+++ pages/api/account/update.ts
@@ -25,19 +25,24 @@ async function updateAccount(
     return;
   }
 
-  const user: UserRecord | false = await getUser(req.decodedToken.uid);
-  if (!user) {
+  const user: UserRecord | Boolean = await getUser(req.decodedToken.uid);
+  if (!(user instanceof UserRecord)) {
     res.status(400).json({ ok: "not ok" });
     res.end();
     return;
   }
 
-  const body = JSON.parse(req.body);
+  let body;
+  try {
+    body = JSON.parse(req.body);
+  } catch (e) {
+    body = {};
+  }
 
   const data: AccountProps = {
     uid: user.uid,
-    name: body.name,
-    prefferedLang: body.prefferedLang,
+    name: body?.name,
+    prefferedLang: body?.prefferedLang,
     confirmedEmail: user.emailVerified,
     confirmedPhone: !!user.phoneNumber,
   };
