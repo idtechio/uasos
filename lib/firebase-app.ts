087fdb3c87c072ae8143af276bd71fe8994180cf
Michal Szkodzinski <mszkodzinski@users.noreply.github.com>
2022-03-25 17:31:46 +0100

    env for building fix

--- lib/firebase-app.ts
+++ lib/firebase-app.ts
@@ -7,7 +7,7 @@ import {
 import { Auth, getAuth } from "firebase/auth";
 
 const options: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "---",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
