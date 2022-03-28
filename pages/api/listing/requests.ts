a3470b6247945f4eb2dbb39a8fd8f98b7df814d6
Michal Szkodzinski <mszkodzinski@users.noreply.github.com>
2022-03-28 19:31:40 +0200

    SOSUA-129 requests list

--- pages/api/listing/requests.ts
+++ pages/api/listing/requests.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { select } from "../../../lib/db";
 import {
   /* withApiAuth, */ ApiAuthTokenDetails,
 } from "../../../src/helpers/withAPIAuth";
@@ -33,6 +34,7 @@ export interface RequestProps {
   is_with_elderly: Boolean;
   is_ukrainian_nationality: Boolean;
   duration_category: Array<string>;
+  match_status?: string;
   matchedOffer?: MatchedOfferProps;
 }
 
@@ -47,9 +49,84 @@ async function getRequests(
   //   return;
   // }
 
-  // TODO get requests details from backend for user req.decodedToken.uid
+  let requests: RequestProps[];
 
-  const requests: RequestProps[] = [
+  if (req.decodedToken?.uid) {
+    requests = await getRequestsFromDB(req.decodedToken.uid);
+  } else {
+    requests = getMockRequests();
+  }
+
+  res.status(200).json({ ok: "ok", requests });
+  res.end();
+}
+
+async function getRequestsFromDB(uid: string): Promise<RequestProps[]> {
+  const guestsList: false | any[] = await select(
+    `SELECT
+      g.db_guests_id as guest_id,
+      g.city, g.country, g.listing_country,
+      g.phone_num, g.email,
+      g.shelter_type, g.beds,
+      g.acceptable_shelter_types,
+      g.group_relation,
+      g.duration_category,
+      g.is_pregnant, g.is_with_disability, g.is_with_animal,
+      g.is_with_elderly, g.is_ukrainian_nationality,
+      g.duration_category,
+      o.fnc_status as guest_status,
+      m.db_matches_id as match_id,
+      m.fnc_status as match_status,
+      h.db_hosts_id as host_id,
+      h.city as host_city,
+      h.country as host_country,
+      h.listing_country as host_listing_country,
+      h.phone_num as host_phone_num,
+      h.email as host_email
+    FROM guests g
+    JOIN accounts a ON a.db_accounts_id = g.fnc_accounts_id
+    LEFT JOIN matches m ON m.fnc_hosts_id = g.db_guests_id
+    LEFT JOIN hosts h ON h.db_hosts_id = m.fnc_hosts_id
+    WHERE a.uid = $1`,
+    [uid]
+  );
+
+  if (!guestsList) {
+    return [];
+  }
+
+  return guestsList.map((g) => ({
+    id: g.guest_id,
+    city: g.city,
+    country: g.country,
+    listing_country: g.listing_country,
+    phone_num: g.phone_num,
+    email: g.email,
+    acceptable_shelter_types: g.acceptable_shelter_types,
+    beds: g.beds,
+    group_relation: g.group_relation,
+    is_pregnant: g.is_pregnant,
+    is_with_disability: g.is_with_disability,
+    is_with_animal: g.is_with_animal,
+    is_with_elderly: g.is_with_elderly,
+    is_ukrainian_nationality: g.is_ukrainian_nationality,
+    duration_category: g.duration_category,
+    match_status: g.match_status,
+    matchedOffer: g.match_id
+      ? {
+          id: g.host_id,
+          city: g.host_city,
+          country: g.host_country,
+          listing_country: g.host_listing_country,
+          phone_num: g.host_phone_num,
+          email: g.host_email,
+        }
+      : undefined,
+  }));
+}
+
+function getMockRequests(): RequestProps[] {
+  return [
     {
       id: "aaa4e25e-aae4-11ec-9a20-1726ed50bb17",
       city: "Warszawa",
@@ -110,9 +187,6 @@ async function getRequests(
       duration_category: ["2_3_weeks"],
     },
   ];
-
-  res.status(200).json({ ok: "ok", requests });
-  res.end();
 }
 
 // TODO turn on auth
