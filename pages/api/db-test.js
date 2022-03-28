496dd8e6ffecfc39ba9a9d97a299b0d00a9f108a
Michal Szkodzinski <mszkodzinski@users.noreply.github.com>
2022-03-28 19:21:27 +0200

    db connection

--- pages/api/db-test.js
+++ pages/api/db-test.js
@@ -1,22 +1,13 @@
-import { Pool } from "pg";
+import { select } from "../../lib/db";
 
 export default async function dbTest(req, res) {
   try {
-    const pool = new Pool({
-      database: "postgres",
-      user: "postgres",
-      password: "postgres",
-      host: "/cloudsql/ukrn-hlpr-test:europe-central2:sql-ukr-helper-test-iter-011",
-      // port: 5432,
-      // ssl: true,
-    });
-
-    const result = await pool.query("SELECT count(*) FROM hosts");
-    const result2 = await pool.query("SELECT * FROM hosts LIMIT 10");
+    const result = await select("SELECT count(*) FROM hosts");
+    // const result2 = await select("SELECT * FROM hosts LIMIT 10");
 
     res.status(200).json({
       result,
-      result2,
+      // result2,
     });
   } catch (error) {
     res.status(400).json({ error });
