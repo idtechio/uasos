e0d3f9c483ccfa496e85ddd9006194190fabe3f1
Michal Szkodzinski <mszkodzinski@users.noreply.github.com>
2022-03-29 08:35:46 +0200

    api request - url fix

--- src/client-api/numbers.ts
+++ src/client-api/numbers.ts
@@ -1,7 +1,9 @@
 import { GetNumberList } from "../../pages/api/listing/numbers";
 
 export const getNumberList = async () => {
-  const res = await fetch("/api/listing/numbers", { method: "GET" });
+  const res = await fetch(window.location.origin + "/api/listing/numbers", {
+    method: "GET",
+  });
 
   if (res.status != 200) {
     throw new Error("Couln't fetch numbers list, try again later.");
