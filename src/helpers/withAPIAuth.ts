e06f1bcd8dad6e9cdfe4e58aa741f598eb2ea1cd
Michal Szkodzinski <mszkodzinski@users.noreply.github.com>
2022-03-30 12:19:20 +0200

    ts fix

--- src/helpers/withAPIAuth.ts
+++ src/helpers/withAPIAuth.ts
@@ -47,7 +47,7 @@ const readAndCheckToken = async function (
     if (_token) {
       token = _token;
       const _decodedToken = await decodeToken(token);
-      if (_decodedToken !== false) {
+      if (!(_decodedToken instanceof Boolean)) {
         decodedToken = _decodedToken;
       }
     }
