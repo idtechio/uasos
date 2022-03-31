9c37144ae319b4e5f20e0238b784d27d702c95ab
Łukasz <lukaszradecki404@gmail.com>
2022-03-31 09:53:24 +0200

    account client api - next public domain env added to request url

--- src/client-api/account.ts
+++ src/client-api/account.ts
@@ -19,24 +19,32 @@ interface AccountApi {
 export const AccountApi: AccountApi = {
   getAccount: async function () {
     const token = await getFirebaseToken();
-    const res = await fetch(`/api/account/get`, {
-      method: "post",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_DOMAIN + `/api/account/get`,
+      {
+        method: "POST",
+        headers: {
+          mode: "no-cors",
+          "content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
     return (await res.json()).account;
   },
   updateAccount: async function ({ payload }) {
     const token = await getFirebaseToken();
-    const res = await fetch("/api/account/update", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_DOMAIN + "/api/account/update",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      }
+    );
 
     return await res.json();
   },
