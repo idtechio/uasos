709069cc5cef3418bf90d920f1e752bd8c784acc
Michal Szkodzinski <mszkodzinski@users.noreply.github.com>
2022-03-31 11:08:54 +0200

    cors for test env

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -3,8 +3,15 @@ import { NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
+
+  // eslint-disable-next-line no-console
+  console.log("middleware start");
+
   if (process.env.ENV_NAME === "test") {
     res.headers.set("Access-Control-Allow-Origin", "*");
+
+    // eslint-disable-next-line no-console
+    console.log("middleware header added");
   }
   return res;
 }
