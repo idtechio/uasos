80a2e361e6c3ca1aa556d2c60a7755583ddebb49
Michal Szkodzinski <mszkodzinski@users.noreply.github.com>
2022-03-31 12:08:21 +0200

    cors for test env

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,5 +1,24 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import Cors from "cors";
+
+export default function initMiddleware(middleware: any) {
+  return (req: unknown, res: unknown) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result: unknown) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+}
+
+const cors = initMiddleware(
+  Cors({
+    origin: "*",
+  })
+);
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
@@ -8,12 +27,13 @@ export async function middleware(req: NextRequest) {
   console.log("middleware start");
 
   if (process.env.ENV_NAME === "test") {
-    res.headers.set("Access-Control-Allow-Credentials", "true");
-    res.headers.set("Access-Control-Allow-Origin", "*");
-    res.headers.set(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-    );
+    await cors(req, res);
+    // res.headers.set("Access-Control-Allow-Credentials", "true");
+    // res.headers.set("Access-Control-Allow-Origin", "*");
+    // res.headers.set(
+    //   "Access-Control-Allow-Methods",
+    //   "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    // );
 
     // eslint-disable-next-line no-console
     console.log("middleware header added");
