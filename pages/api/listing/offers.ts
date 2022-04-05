a7c07a07746687db0e2ab3cfefe953fdfa3347a4
Artem Zhabinsky <artem.zhabinsky@start-up.house>
2022-04-05 16:05:51 +0200

    SOSUA-177 Create a update screen based on "create a new" one

--- pages/api/listing/offers.ts
+++ pages/api/listing/offers.ts
@@ -78,7 +78,7 @@ export interface OfferProps {
   ok_for_animals: Boolean;
   ok_for_elderly: Boolean;
   ok_for_any_nationality: Boolean;
-  duration_category: Array<string>;
+  duration_category: Array<string> | string;
   type: GuestHostType;
   transport_included: Boolean;
   match_id?: string | null;
@@ -209,7 +209,7 @@ async function getOffersFromDB(uid: string): Promise<OfferProps[]> {
     ok_for_animals: h.ok_for_animals,
     ok_for_elderly: h.ok_for_elderly,
     ok_for_any_nationality: h.ok_for_any_nationality,
-    duration_category: h.duration_category,
+    duration_category: ungroupString(h.duration_category),
     transport_included: h.transport_included,
     type: h.type,
     match_id: h.match_id,
