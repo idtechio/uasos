0318990b738b97d122c33cda2f21ff91d58a8f36
Anton Franchuk <afranchuk@blackberry.com>
2022-04-06 23:20:02 +0300

    Add google tag and refactor pixel

--- pages/gtag.tsx
+++ pages/gtag.tsx
@@ -4,7 +4,7 @@ export type GtagProps = {
   id: string;
 };
 
-const Gtag = ({ id }: GtagProps) => {
+export const GAtag = ({ id }: GtagProps) => {
   return (
     <>
       <Script
@@ -12,7 +12,7 @@ const Gtag = ({ id }: GtagProps) => {
         src={`https://www.googletagmanager.com/gtag/js?id=${id}`}
       />
       <Script
-        id="gtag-init"
+        id="gatag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -29,4 +29,21 @@ const Gtag = ({ id }: GtagProps) => {
   );
 };
 
-export default Gtag;
+export const Gtag = ({ id }: GtagProps) => {
+  return (
+    // Google Tag Manager
+    <Script
+      id="gtag"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${id}');
+          `,
+      }}
+    />
+  );
+};
