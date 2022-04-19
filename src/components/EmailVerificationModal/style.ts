1a52045fa358ed3d00b02677d51dc6f63517c236
RostyslavMykhalskyyIntive <rostyslav.mykhalskyy@intive.com>
2022-04-19 15:28:32 +0300

    fix: modal

--- src/components/EmailVerificationModal/style.ts
+++ src/components/EmailVerificationModal/style.ts
@@ -1,31 +1,32 @@
 import styled from "styled-components/native";
+import { Theme } from "../../style/theme.config";
 
-export const StyledText = styled.Text`
-  font-size: 16px;
+type Props = {
+  disabled?: boolean;
+  theme: Theme;
+};
+export const StyledText = styled.Text<Props>`
+  font-size: 14px;
   font-weight: 400;
-  color: #003566;
+  color: ${({ disabled, theme }) =>
+    disabled ? theme.colors.alert : theme.colors.text};
+  margin: 20px 0;
+  text-align: center;
+`;
+export const StyledTextButton = styled.Text<Props>`
+  font-size: 16px;
+  font-weight: 700;
+  color: ${({ disabled, theme }) =>
+    disabled ? "#D9D9D9" : theme.colors.headings};
   margin: 20px 0;
   text-align: center;
 `;
 export const StyledHeader = styled.Text`
   font-size: 24px;
-  color: #003566;
+  color: ${({ theme }: { theme: Theme }) => theme.colors.headings};
   font-weight: 700;
   margin: 20px 0;
   text-align: center;
-`;
-export const InputWrapper = styled.View`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-`;
-export const StyledInput = styled.TextInput`
-  width: 35;
-  height: 50;
-  border: 1px solid lightgrey;
-  font-size: 24px;
-  text-align: center;
-  margin: 0 5px;
 `;
 export const Wrapper = styled.View`
   display: flex;
