03095741742b0990c63099ab797f532c24cdbb9e
Anton Franchuk <afranchuk@blackberry.com>
2022-05-04 19:37:49 +0300

    Remove unused Autocomplete component

--- src/queries/useAutocomplete.ts
+++
@@ -1,6 +0,0 @@
-import { useQuery } from "react-query";
-import { QueryKeys } from "./queryKeys";
-import { fetchAutocomplete } from "../client-api/autocomplete";
-
-export const useAutocomplete = (query: string) =>
-  useQuery([QueryKeys.AUTOCOMPLETE, query], () => fetchAutocomplete(query));
