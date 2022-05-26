6f516ad333b98a9f17b357a67212ed49723346c0
Anton Franchuk <afranchuk@blackberry.com>
2022-05-26 14:04:04 +0300

    Add wordings module with description text

---
+++ src/components/IdCheck/wordings.ts
@@ -0,0 +1,4 @@
+export const wordings = {
+  "default.doc.descriptions.doc":
+    "Please upload photo or scan of your valid ID or passport. Make sure the photo is readable and without glare or blurred areas.",
+};
