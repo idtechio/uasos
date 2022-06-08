afd65f887252d46f65b96c943811408f26472d07
Stas Kovalov <stanislav.kovalov@intive.com>
2022-06-09 01:59:01 +0300

    test eslint

--- packages/app/screens/home/index.tsx
+++ packages/app/screens/home/index.tsx
@@ -5,9 +5,6 @@ import { ButtonDefault } from "app/components/Buttons";
 import { ButtonCta } from "app/components/Buttons";
 import { ButtonSM } from "app/components/Buttons";
 
-console.log("work");
-const a = "s";
-
 export function HomeScreen() {
   return (
     <SafeAreaView
