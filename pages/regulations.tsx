6694b6fdd38e4e0f2bdc0ab9792591f763027b73
SergeyKulakovintive <serhii.kulakov@intive.com>
2022-06-14 13:12:47 +0300

    return dangerouslySetInnerHTML

--- pages/regulations.tsx
+++ pages/regulations.tsx
@@ -4,7 +4,6 @@ import {
 } from "../src/components/Compositions";
 import { useTranslation } from "next-i18next";
 import { GetServerSideProps } from "next";
-import styled from "styled-components/native";
 import { withSession } from "../src/helpers/withSession";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { Routes } from "../src/consts/router";
@@ -17,17 +16,15 @@ const RegulationsPage = () => {
     <CompositionAppBody>
       <AppBack to={Routes.HOMEPAGE} />
       <CompositionSection padding={[40, 15, 40, 15]} flexGrow="2">
-        <InnerHTML>
-          <InnerHTMLText>{t("regulations")}</InnerHTMLText>
-        </InnerHTML>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: t("regulations"),
+          }}
+        />
       </CompositionSection>
     </CompositionAppBody>
   );
 };
-
-const InnerHTML = styled.View``;
-
-const InnerHTMLText = styled.Text``;
 
 export const getServerSideProps: GetServerSideProps = withSession(
   async ({ locale }, session) => ({
