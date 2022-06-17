29e2a9b99da3638f2bc28a90d97ff5860d08a1ca
SergeyKulakovintive <serhii.kulakov@intive.com>
2022-06-17 11:48:13 +0300

    fix ppack never used

--- packages/app/screens/home/index.tsx
+++ packages/app/screens/home/index.tsx
@@ -2,11 +2,8 @@ import React from "react";
 import { SafeAreaView } from "app/provider/safe-area";
 import { ButtonDefault } from "app/components/Buttons";
 import { ButtonCta } from "app/components/Buttons";
-import { ButtonSM } from "app/components/Buttons";
-import AppBack from "app/components/AppBack";
 import Section from "app/components/Section";
 import SectionTitle from "app/components/SectionTitle";
-import { Splash } from "app/components/Splash";
 
 const containerStyle = {
   width: "100%",
@@ -20,11 +17,8 @@ export function HomeScreen() {
         flex: 1,
       }}
     >
-      <ButtonSM id="google" anchor="ButtonSM - google" />
-      <ButtonSM id="facebook" anchor="ButtonSM - facebook" />
       <Section bgColor="gray" style={containerStyle} />
       <SectionTitle title="SectionTitle" style={containerStyle} />
-      <Splash />
       <ButtonCta anchor="ButtonCta" />
       <ButtonDefault anchor="ButtonDefault" />
     </SafeAreaView>
